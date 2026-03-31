10a2eaee2f9f8bcecc721aca00d3aec91f92bdc6
PEDRO.SOUSA <pedro.sousa@liderassessoria.com.br>
2026-03-31 15:04:49 -0300

    Fix group validation

---
+++ backend/src/main/java/com/zmaisz/automator/exception/user/usergrop/UserGroupDisabledException.java
@@ -0,0 +1,9 @@
+package com.zmaisz.automator.exception.user.usergrop;
+
+public class UserGroupDisabledException extends RuntimeException {
+
+    public UserGroupDisabledException(String message) {
+        super(message);
+    }
+
+}
