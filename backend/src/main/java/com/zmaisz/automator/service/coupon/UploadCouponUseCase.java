c84c05c730ce1099a9482731d6e35cc7c311e18c
PEDRO.SOUSA <pedro.sousa@liderassessoria.com.br>
2026-04-02 17:02:21 -0300

    Correct concurrency error with path and unexpected exception.

--- backend/src/main/java/com/zmaisz/automator/service/coupon/UploadCouponUseCase.java
+++ backend/src/main/java/com/zmaisz/automator/service/coupon/UploadCouponUseCase.java
@@ -55,10 +55,12 @@ public class UploadCouponUseCase {
                 continue;
             }
 
-            Path filePath = groupDir.resolve(originalFilename);
-            multipartFile.transferTo(filePath.toFile());
-
             String code = extractCode(originalFilename);
+            String extension = originalFilename.contains(".") ? originalFilename.substring(originalFilename.lastIndexOf(".")) : "";
+            String uniqueFilename = code + "__" + System.currentTimeMillis() + extension;
+
+            Path filePath = groupDir.resolve(uniqueFilename);
+            multipartFile.transferTo(filePath.toFile());
 
             Coupon coupon = new Coupon();
             coupon.setCode(code);
